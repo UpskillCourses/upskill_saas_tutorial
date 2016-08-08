4dd40d072aee512b813e465657e63e19c2d4cce3
Rob Dey <instructors@upskillcourses.com>
2016-08-08 22:23:25 +0000

    Added contact form view, route, controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,4 @@
+class ContactsController < ApplicationController
+  def new
+  end       
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  resources :contacts
 end
