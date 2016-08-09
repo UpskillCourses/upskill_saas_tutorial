e3594625be58f195838fa82f82484c2a559de87d
Rob Dey <instructors@upskillcourses.com>
2016-08-09 23:11:27 +0000

    Improved flash style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
-      flash[:error] = @contact.errors.full_messages.join(", ")
+      flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
