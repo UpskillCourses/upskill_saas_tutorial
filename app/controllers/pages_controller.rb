ba24e46ffc8dbfba24f4c28bc452b8dff296bbd4
Rob Dey <instructors@upskillcourses.com>
2016-09-30 22:23:58 +0000

    Added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+  # GET request for / which is our home page
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def about
