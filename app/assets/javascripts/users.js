0866804c85c0585a27e869afe4c265746630899e
Rob Dey <instructors@upskillcourses.com>
2016-10-03 22:35:52 +0000

    Added js pseudocode for payments

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,6 @@
+//When user clicks form submit btn
+//prevent default submission behavior.
+//Use Stripe JS library to check for errors.
+//Collect the credit card fields.
+//Send card info to Stripe
+//Handle Stripe's response (which includes token)
