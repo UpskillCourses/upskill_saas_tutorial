63d29487c7fc07fcc38020bf4ad5f84a4a44a05f
Rob Dey <instructors@upskillcourses.com>
2016-10-04 01:03:48 +0000

    Send card info to Stripe servers

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,9 @@
-
-//Use Stripe JS library to check for errors.
-//Collect the credit card fields.
 //Send card info to Stripe
 //Handle Stripe's response (which includes token)
 
 //Wait for turbolinks to load, instead of document ready
 $(document).on('turbolinks:load', function(){
+  /* global $, Stripe */
   //Select the form and submit button
   var theForm = $('#pro_form');
   var submitBtn = $('#form-submit-btn');
@@ -17,34 +15,44 @@ $(document).on('turbolinks:load', function(){
     //When user clicks form submit btn
     //prevent default submission behavior.
     event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
     
-    //Get the card inputs
+    //Collect the credit card fields.
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
     
+    //Use Stripe JS library to check for errors.
     var error = false;
     
+    //Validate card number
     if(!Stripe.card.validateCardNumber(ccNum)) {
       error = true;
       alert('The credit card number appears to be invalid.');
     }
     
+    //Validate CVC number
     if(!Stripe.card.validateCVC(cvcNum)) {
       error = true;
       alert('The CVC number appears to be invalid.');
     }
     
+    //Validate expiration date
     if(!Stripe.card.validateExpiry(expMonth, expYear)) {
       error = true;
       alert('The expiration date appears to be invalid.');
     }
     
     if (error) {
-      //don't proceed
+      submitBtn.prop('disabled', false).val("Sign Up");
     } else {
-      //Submit the card info to Stripe
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
     }
   });
 });
