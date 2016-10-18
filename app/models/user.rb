7b6263c498b31d8e16afe8d546c2fe4670cd55cc
Rob Dey <instructors@upskillcourses.com>
2016-10-18 00:47:19 +0000

    Added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   # If Pro user passes validations (email, password, etc.),
