414a2f62dbe4370b97a7b41706356d7947d8d98e
Rob Dey <instructors@upskillcourses.com>
2016-10-18 00:36:12 +0000

    Added profiles table

---
+++ db/migrate/20161018003300_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.integer :user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :job_title
+      t.string :phone_number
+      t.string :contact_email
+      t.text :description
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004195404) do
+ActiveRecord::Schema.define(version: 20161018003300) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20161004195404) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
