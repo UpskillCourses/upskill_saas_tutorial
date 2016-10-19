79b033f96dc795ef45eb5b49587a9bd77b1de929
Rob Dey <instructors@upskillcourses.com>
2016-10-19 05:23:43 +0000

    Revert "Added profile form fields"
    This reverts commit 404c025a9fe35ccdc652b610a4b09ca86e8d4de3.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,5 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
-    @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
   end
 end
