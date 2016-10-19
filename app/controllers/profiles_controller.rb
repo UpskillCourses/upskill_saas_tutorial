80f5818088252aee59b3ee3185dd392b5757ab91
Rob Dey <instructors@upskillcourses.com>
2016-10-19 21:49:53 +0000

    Added file field for image upload to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,6 @@ class ProfilesController < ApplicationController
   
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
 end
