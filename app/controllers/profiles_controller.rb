404c025a9fe35ccdc652b610a4b09ca86e8d4de3
Rob Dey <instructors@upskillcourses.com>
2016-10-19 01:32:29 +0000

    Added profile form fields

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,7 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
   end
 end
