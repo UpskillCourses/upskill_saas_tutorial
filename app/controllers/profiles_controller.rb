687f085f67a57747687795425ac12ffc397b8076
Rob Dey <instructors@upskillcourses.com>
2016-10-20 05:29:36 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,12 @@ class ProfilesController < ApplicationController
     end
   end
   
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
